81564a0860157f14325f8041afab6d1211500f51
Cindy Wong <cindywongdev@gmail.com>
2023-01-11 16:09:10 -0500

    created aboutLoader

--- src/loaders.js
+++ src/loaders.js
@@ -6,6 +6,11 @@ export const projectsLoader = async () => {
     const response = await fetch(URL + "/projects")
     // .json() method turns json object back into JS object
     const projects = await response.json()
-    console.log(projects)
     return projects
+}
+
+export const aboutLoader = async () => {
+    const response = await fetch(URL + "/about")
+    const about = await response.json
+    return about
 }
