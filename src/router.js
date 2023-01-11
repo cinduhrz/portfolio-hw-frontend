3146204159a338ffc7cffe952a778a5f8ddb5974
Cindy Wong <cindywongdev@gmail.com>
2023-01-11 15:34:54 -0500

    app renders header, footer, and routes

--- src/router.js
+++ src/router.js
@@ -5,11 +5,16 @@ import {
     Route
 } from 'react-router-dom'
 import App from './App'
+import Home from './pages/Home'
+import About from './pages/About'
+import Projects from './pages/Projects'
 
 const router = createBrowserRouter(
     createRoutesFromElements(
         <Route path="/" element={<App/>}>
-
+            <Route path="" element={<Home/>}/>
+            <Route path="about" element={<About/>}/>
+            <Route path="projects" element={<Projects/>}/>
         </Route>
     )
 )
